20fb3482238e34158ee53932da5ba5baf92cd39f
Kendrick Parks <kendrick@homdna.com>
2015-09-18 14:09:23 -0700

    removed unused files, moved document parser files to separates folder, started working on cmd line tool

--- main.go
+++ main.go
@@ -2,18 +2,100 @@ package main
 
 import (
 	"fmt"
-	"github.com/kparks29/Document_Parser/documentParser"
+	// "github.com/kparks29/Document_Parser/documentParser"
+	// "github.com/kparks29/Document_Parser/mismo"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"github.com/homdna/homdna-models"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	// 1) from command line get file_location, mimetype, standard, homdna_uuid
-	fmt.Printf("\n %#v", documentParser)
+	if len(os.Args) < 5 {
+		log.Fatalln("\n Missing Arguments! Need the following:  File Location, MIMEtype, Standard, and Homdna Uuid.")
+	}
+	filePath, mimeType, standard, homdna_uuid := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
+
+	serviceApiKey, err := GetApiKey()
+	if err != nil {
+		log.Fatalln(err)
+	}
 	// 2) get homdna version
+	homdna, err := GetLatestHomdna(homdna_uuid, *serviceApiKey)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("\n homdna: %#v \n\n", *homdna)
+
 	// 3) parse
 	// 4) iterate through parsed files
 	// 5) pos document & file
 	// 6) add file to homdna model
 	// 7) post new version
+}
+
+type Config struct {
+	ServiceApiKey string `json:"service_api_key"`
+}
+
+func (this *Config) Validate() error {
+	if this.ServiceApiKey == "" {
+		return errors.New("A valid api key must be set. The hash key is used by other microservices to make requests to special APIs not intended for public consumption.")
+	}
+	return nil
+}
+
+func LoadConfig(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configData := &Config{}
+	if err := decoder.Decode(configData); err != nil {
+		return nil, err
+	}
+	return configData, nil
+}
+
+func GetApiKey() (*string, error) {
+	config, err := LoadConfig("./document_parser.conf")
+	if err != nil {
+		return nil, errors.New("Error loading configuration file.")
+	}
+	if err = config.Validate(); err != nil {
+		return nil, err
+	}
+
+	serviceApiKey := &config.ServiceApiKey
+	return serviceApiKey, nil
+}
+
+func GetLatestHomdna(homdnaId string, serviceApiKey string) (*models.HomdnaModel, error) {
+	url := "https://dev.homdna.com/homdnas/" + homdnaId + "/versions/latest"
+	payload := []byte{}
+	fmt.Printf("\n %v \n %v \n %v \n %v \n\n", "GET", url, payload, serviceApiKey)
+	response, err := makeRequest("GET", url, &payload, &serviceApiKey)
+	if err != nil {
+		return nil, err
+	}
+	homdnaResponse, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	homdna := models.HomdnaModel{}
+	err = json.Unmarshal(homdnaResponse, &homdna)
+	if err != nil {
+		return nil, err
+	}
+	return &homdna, nil
 }
 
 // ALREADY WRITTEN MOST OF THIS HERE
@@ -27,20 +109,26 @@ func main() {
 // 	"github.com/homdna/homdna-models"
 // 	"github.com/homdna/homdna-service/domain"
 // 	"github.com/homdna/homdna-service/requests"
-// 	"github.com/kparks29/Document_Parser/mismo"
 // 	"io/ioutil"
-// 	"log"
 // 	"net/http"
-// 	"os"
 // 	"strings"
 // )
 
 // func main() {
 
-// 	file, err := appraisal.ReadFile(os.Args[1])
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
+// xmlFile, err := os.Open(filePath)
+// if err != nil {
+// 	log.Fatalln(err)
+// }
+
+// defer xmlFile.Close()
+
+// readFile, err := ioutil.ReadAll(xmlFile)
+// if err != nil {
+// 	log.Fatalln(err)
+// }
+
+// return &readFile, nil
 
 // 	// PASS IN A FILE TO RECEIVE A HOMDNA BACK (WORKFLOW 1)
 // 	appraisalResponse, err := appraisal.UpdateHomdnaModel(file)
@@ -149,22 +237,22 @@ func main() {
 // 	return
 // }
 
-// func makeRequest(method string, url string, payload *[]byte, serviceApiKey *string) (*http.Response, error) {
-// 	client := &http.Client{}
-// 	request, err := http.NewRequest("POST", url, bytes.NewReader(*payload))
-// 	request.Header["Content-Type"] = []string{"application/json"}
-// 	request.Header["X-Service-Api-Key"] = []string{*serviceApiKey}
-// 	response, err := client.Do(request)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	if response.StatusCode != http.StatusOK {
-// 		body, _ := ioutil.ReadAll(response.Body)
-// 		fmt.Printf("Status code: %v Status: %v RESPONSE: %v", response.StatusCode, response.Status, string(body))
-// 		return nil, errors.New("Bad Status Code")
-// 	}
-// 	return response, nil
-// }
+func makeRequest(method string, url string, payload *[]byte, serviceApiKey *string) (*http.Response, error) {
+	client := &http.Client{}
+	request, err := http.NewRequest(method, url, bytes.NewReader(*payload))
+	request.Header["Content-Type"] = []string{"application/json"}
+	request.Header["X-Service-Api-Key"] = []string{*serviceApiKey}
+	response, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(response.Body)
+		fmt.Printf("Status code: %v Status: %v RESPONSE: %v", response.StatusCode, response.Status, string(body))
+		return nil, errors.New("Bad Status Code")
+	}
+	return response, nil
+}
 
 // func CreateAccount(email string, fullName string, serviceApiKey *string) *requests.AccountCreationRequest {
 // 	firstName, lastName := parseName(fullName)
